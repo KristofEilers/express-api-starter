e9a5e5155cb498c75d726874ed229dc7fc487aa8
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-06-08 10:38:21 +0200

    setting up starter with user

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 try {
   const client = await mongoose.connect(process.env.MONGO_URI);
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// import "./db/index.js";
-import express from "express";
-import cors from "cors";
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const db = require("./db/index.js");
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+// db();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("*", (req, res) => res.sendStatus(404));
-// app.use(errorHandler);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`)
